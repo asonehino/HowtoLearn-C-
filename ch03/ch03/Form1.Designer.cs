702115bf78d08c5370f179a63eff6b4c26523ba4
hino <asonehino@naver.com>
2024-10-10 16:18:53 +0900

    mqtt

---
+++ ch03/ch03/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ch03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(22, 17);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(250, 395);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(293, 17);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(483, 395);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "토픽";
+            this.columnHeader1.Width = 178;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "페이로드";
+            this.columnHeader2.Width = 189;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "2311박현호";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ch03/ch03/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace ch03
+{
+    public partial class Form1 : Form
+    {
+        MqttClient client;
+        string clientId;
+        
+        Dictionary<string, string> mydic = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MQTT 브로커와 연결하는 부분
+            string BrokerAddress = "broker.emqx.io";
+            client = new MqttClient(BrokerAddress);
+
+            // register a callback-function (we have to implement, see below) which is called by the library when a message was received
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            // use a unique id as client id, each time we start the application
+            clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            //연결완료지점 = 구독하는 지점
+            //bssm/#은 bssm/로 시작하는 거 모두 받기
+            client.Subscribe(new string[] { "bssm/#" }, new byte[] { 0 });
+        }
+
+        //MQTT이벤트 핸들러
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string ReceivedMessage = Encoding.UTF8.GetString(e.Message);
+            richTextBox1.Text += "[" + e.Topic + "]"  + ReceivedMessage + "\n";
+
+            //지금 날라온 토픽을 사전에서 검색한다
+            //검색결과 사전에 있다면 value를 업데이트
+            //검색결과 사전에 없다면 최초로 온 메시지이므로 추가
+            if (mydic.ContainsKey(e.Topic))
+            {
+                //이미 받은 적이 있으면
+                mydic[e.Topic] = ReceivedMessage;
+            }
+            else
+            {
+                //최초로 수신한 토픽
+                mydic.Add(e.Topic, ReceivedMessage);
+            }
+        }
+
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //MQTT 연결 종료
+            client.Disconnect();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //타이머가 1초 간격으로 무엇을 하냐?
+            //딕셔너리안에 있는 내용을 몽땅 listview1에 출력
+            //기존내용을 다 삭제하고 새로운 내용을 추가
+            listView1.Items.Clear();
+            foreach (KeyValuePair<string, string> entry in mydic)
+            {
+                //entry.Key
+                //entry.Value
+                //리스트 뷰의 데이터를 한 레코드에 집어넣는 자료형 만들기
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = entry.Key; //토픽
+                lvi.SubItems.Add(entry.Value); //페이로더
+
+                listView1.Items.Add(lvi);
+            }
+        }
+    }
+}
